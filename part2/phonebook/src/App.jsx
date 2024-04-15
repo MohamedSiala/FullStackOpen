99a8e1eb8a85c5b3fbf116310ded24a768264a6a
MohamedSiala <mohamed.siala98@gmail.com>
2024-04-15 16:40:16 +0200

    part2.d

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import personsService from "./services/persons";
 import Filter from "./Filter";
 import PersonForm from "./PersonForm";
 import Persons from "./Persons";
@@ -11,23 +11,65 @@ const App = () => {
   const [searchName, setSearchName] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
-    });
+    personsService.getAll().then((response) => setPersons(response));
   }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
-    if (!persons.map((person) => person.name).includes(newName)) {
+    if (
+      !persons
+        .map((person) => person.name.toLocaleLowerCase())
+        .includes(newName.toLocaleLowerCase())
+    ) {
       const personToAdd = {
         name: newName,
         number: newNumber,
-        id: persons.length + 1,
       };
-      setPersons(persons.concat(personToAdd));
-      setNewName("");
-      setNewNumber("");
-    } else alert(`${newName} is already added to phonebook`);
+      personsService.create(personToAdd).then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+        setNewName("");
+        setNewNumber("");
+      });
+    } else {
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        const person = persons.find(
+          (person) => person.name.toLowerCase() === newName.toLocaleLowerCase()
+        );
+        const updatedPerson = { ...person, name: newName, number: newNumber };
+        personsService
+          .update(person.id, updatedPerson)
+          .then((returnedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id === returnedPerson.id ? returnedPerson : person
+              )
+            );
+            setNewName("");
+            setNewNumber("");
+          })
+          .catch(() => {
+            alert(
+              `the person '${person.name}' was already deleted from server`
+            );
+          });
+      }
+    }
+  };
+
+  const handleDelete = (id) => {
+    let personToDelete = persons.find((person) => person.id === id);
+    if (window.confirm(`Delete ${personToDelete.name} ?`)) {
+      personsService
+        .remove(id)
+        .then((response) =>
+          setPersons(persons.filter((person) => person.id !== response.id))
+        )
+        .catch(() => alert(`person was already deleted from server`));
+    }
   };
 
   const handleNameChange = (event) => {
@@ -57,7 +99,7 @@ const App = () => {
       />
       <h2>Numbers</h2>
 
-      <Persons personsToShow={personsToShow} />
+      <Persons personsToShow={personsToShow} handleDelete={handleDelete} />
     </div>
   );
 };
--- part2/phonebook/src/Persons.jsx
+++ part2/phonebook/src/Persons.jsx
@@ -1,10 +1,14 @@
 import Person from "./Person";
 
-const Persons = ({ personsToShow }) => {
+const Persons = ({ personsToShow, handleDelete }) => {
   return (
     <div>
       {personsToShow.map((person) => (
-        <Person key={person.id} person={person} />
+        <Person
+          key={person.id}
+          person={person}
+          handleDelete={() => handleDelete(person.id)}
+        />
       ))}
     </div>
   );
