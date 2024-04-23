e9b5a04e5c6fc4ef1fdd54fb179bd9a288519cc2
MohamedSiala <mohamed.siala98@gmail.com>
2024-04-23 19:01:58 +0200

    part4.a

---
+++ part4/app.js
@@ -0,0 +1,25 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const Blog = require("./models/blog");
+const morgan = require("morgan");
+morgan.token("data", function (req) {
+  if (Object.keys(req.body).length !== 0) {
+    return JSON.stringify(req.body);
+  }
+  return " ";
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
+app.use("/api/blogs", blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+module.exports = app;
